ab39e8af5803f12dc429688645758d2d01b1a89f
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-04-02 20:22:08 -0300

    initial commit.

---
+++ patrimoniomais/src/test/java/com/francisco/patrimoniomais/PatrimoniomaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.patrimoniomais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoniomaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
