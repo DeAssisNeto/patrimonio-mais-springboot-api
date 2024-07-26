d3318d195163335368a166800aa258c727709df1
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-07-26 15:06:35 -0300

    feat: created endpoint GET ALL image.

--- patrimoniomais/src/main/java/com/francisco/patrimoniomais/controllers/ImageController.java
+++ patrimoniomais/src/main/java/com/francisco/patrimoniomais/controllers/ImageController.java
@@ -1,9 +1,30 @@
 package com.francisco.patrimoniomais.controllers;
 
+import com.francisco.patrimoniomais.models.ImageModel;
+import com.francisco.patrimoniomais.services.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
+    @Autowired
+    private ImageService imageService;
+
+
+
+
+    @GetMapping
+    public ResponseEntity<Page<ImageModel>> getAll(@PageableDefault(
+            page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC
+    ) Pageable pageable) {
+        return ResponseEntity.ok(imageService.findAll(pageable));
+    }
 }
--- patrimoniomais/src/main/java/com/francisco/patrimoniomais/services/ImageService.java
+++ patrimoniomais/src/main/java/com/francisco/patrimoniomais/services/ImageService.java
@@ -1,11 +1,18 @@
 package com.francisco.patrimoniomais.services;
 
+import com.francisco.patrimoniomais.models.ImageModel;
 import com.francisco.patrimoniomais.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageService {
     @Autowired
     private ImageRepository imageRepository;
+
+    public Page<ImageModel> findAll(Pageable pageable) {
+        return imageRepository.findAll(pageable);
+    }
 }
