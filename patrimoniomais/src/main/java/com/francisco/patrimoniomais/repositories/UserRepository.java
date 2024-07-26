22d7c95ac3a265f4dc6f01b2c333b65cc43909d3
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-07-26 14:30:19 -0300

    feat: ImageRepository, ImageService and ImageController created.

--- patrimoniomais/src/main/java/com/francisco/patrimoniomais/repositories/UserRepository.java
+++ patrimoniomais/src/main/java/com/francisco/patrimoniomais/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.UUID;  
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
